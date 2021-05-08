305aa80ab8736bf3bfd2d5f262dfaa5d799634ee
Anurag9497 <anurag.thakur495@gmail.com>
2021-05-08 22:54:01 +0530

    Update commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Investment from './components/Investment';
 import MaskCopy from './components/MaskCopy';
+import Rectangle from './components/Rectangle';
+import RectangleBelow from './components/RectangleBelow';
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <MaskCopy />
+      <Rectangle />
+      <RectangleBelow />
+      <Investment />
     </div>
   );
 }
